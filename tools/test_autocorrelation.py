b5c077b3bc35eefcb87e8f1c6094698171626d48
Ballarotta Maxime <mballarotta@cls.fr>
2018-07-27 10:16:03 +0200

    update test_autocorrelation.py

--- tools/test_autocorrelation.py
+++ tools/test_autocorrelation.py
@@ -33,7 +33,7 @@ def ahran_model(x_zero_crossing, display=True):
     return corr_model
 
 
-def compute_autocorrelation(psd_x, wavenumber_x, display=True):
+def compute_autocorrelation(psd_x, wavenumber_x, x_zero_crossing, display=True):
     """
 
     :param psd_x:
@@ -45,10 +45,17 @@ def compute_autocorrelation(psd_x, wavenumber_x, display=True):
     autocorrelation = np.fft.ifft(psd_x).real
     autocorrelation /= autocorrelation[0]
     delta_f = 1 / (wavenumber_x[-1] - wavenumber_x[-2])
-    distance = np.linspace(0, int(delta_f/2), int(autocorrelation.size /2 ))
+    distance = np.linspace(0, int(delta_f/2), int(autocorrelation.size /2))
+
+    corr_model = (1 + factor_a * x / x_zero_crossing + one_six * (factor_a * x / x_zero_crossing) ** 2 -
+                  one_six * (factor_a * x / x_zero_crossing) ** 3) * \
+                 np.exp(-np.abs(factor_a * x / x_zero_crossing))
+
 
     if display:
-        plt.plot(distance, autocorrelation[:int(autocorrelation.size/2)], lw=2, color='r')
+        plt.plot(distance, autocorrelation[:int(autocorrelation.size/2)], lw=2, color='b', label='autocorrelation')
+        plt.plot(x, corr_model, lw=2, color='r', label='MODEL')
+        plt.legend()
         plt.xlabel("DISTANCE (km)")
         plt.ylabel("AUTOCORRELATION")
         plt.axhline(y=0., color='k', lw=2)
@@ -84,4 +91,4 @@ def power_spectrum(correlation_model, display=True):
 
 correlation = ahran_model(zero_crossing)
 wavenumber, psd = power_spectrum(correlation)
-compute_autocorrelation(psd, wavenumber)
+compute_autocorrelation(psd, wavenumber, zero_crossing)
